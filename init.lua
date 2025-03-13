d287f83b6d88a5fa2af62e2ccb34473bac48a093
Alice Lia Stapleton <slimelia@protonmail.com>
2025-03-13 16:13:47 +0000

    Remove autoformat for Python
    I might just remove this plugin entirely

--- init.lua
+++ init.lua
@@ -750,7 +750,7 @@ require('lazy').setup({
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
+        local disable_filetypes = { c = true, cpp = true, python = true }
         local lsp_format_opt
         if disable_filetypes[vim.bo[bufnr].filetype] then
           lsp_format_opt = 'never'
@@ -890,7 +890,6 @@ require('lazy').setup({
       }
     end,
   },
-
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
